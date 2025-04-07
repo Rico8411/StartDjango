b1fed4681540d1eaa923ef6e343ee1334654f3e0
Rico8411 <lm407479@gmail.com>
2025-04-07 17:18:59 +0330

    last commit in today

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+
+def product(request):
+    return render(request, "product.html")
+
+
+def store(request):
+    return render(request, "store.html")
+
+
+def hekout(request):
+    return render(request, "hekout.html")
