7c89e8b662f47e0a217a93f70e7f8652603f32cf
Rico8411 <lm407479@gmail.com>
2025-05-26 21:51:52 +0330

    add unique together

--- shop/models.py
+++ shop/models.py
@@ -19,12 +19,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
-    
+
 class Cart(models.Model):
-    quantity = models.PositiveIntegerField()
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    # sbad kharid
+
 class Order(models.Model):
     total_price = models.DecimalField(decimal_places=2, max_digits=10)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -33,8 +34,8 @@ class Order(models.Model):
     
     
 class OrderProduct(models.Model):
-    Order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, default=None, null=True)
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(decimal_places=2, max_digits=10)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -47,5 +48,9 @@ class PaymentLog(models.Model):
     user_id = models.PositiveIntegerField()
     status = models.CharField(max_length=100)
     error_code = models.CharField(max_length=100)
+
+class Meta:
+    unique_together = ('user', 'product')
+
     
 # Create your models here.
